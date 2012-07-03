95d900ad63c5b9c6801fa436024c8043762d68ad
Chris Powell <chris.i.powell@gmail.com>
2012-07-03 17:29:49 +0100

    Multiple tracks and improved rendering

--- BRAGViewer.js
+++ BRAGViewer.js
@@ -1,7 +1,7 @@
 /**
  * Bacterial Reference Annotation Genome Viewer
- @namespace BRAGV
-*/
+ * @namespace BRAGV
+ */
 var BRAGV = {};
 
 var debug = true;
@@ -15,23 +15,44 @@ BRAGV.Viewer = function(divName)
 	this.offset = 0;
 	this.numBases = 100000;
 	
+	this.c_length = 1000000;
+	
+	this.lastDraw = 0;
+	
 	this.start = 0;
 	this.end = 0;
 	
 	this.tracks = {};
 	
-	div = $(document.getElementById(divName));
+	this.labelWidth = 40.0;
 	
-	var w = div.innerWidth();
-	var h = 400;
+	var div = $(document.getElementById(divName));
 	
-	div.append('<canvas id='+ divName + '_Viewer width="'+w+'" height="'+h+'"></canvas>');
+	var w = div.innerWidth() - 25;
+	var h = 200;
 	
-	ctx = $('canvas', div)[0].getContext('2d');
+	div.append('<canvas id='+ divName + '_Viewer width="'+w+'" height="'+h+'" class="bragv_canvas"></canvas>');
 	
-	this.baseWidth = (w - 40.0) / this.numBases;
-	this.trackWidth = w - 40;
+	this.ctx = $('canvas', div)[0].getContext('2d');
 	
+	this.baseWidth = (w - this.labelWidth) / this.numBases;
+	this.trackWidth = w - this.labelWidth;
+	
+	this.horizontalScroller = new BRAGV.Scroller(divName, 'h');
+	this.horizontalScroller.step = 1;
+	this.verticalScroller = new BRAGV.Scroller(divName, 'v');
+	this.verticalScroller.step = 20;
+	var vwr = this;
+	
+	this.horizontalScroller.onValueChange = function(val)
+	{
+		
+		vwr.setOffset(val);
+	};
+	this.verticalScroller.onValueChange = function(val)
+	{
+		vwr.setZoom(val);
+	};
 	/*if(debug)
 	{
 		this.tracks["track1"] = new BRAGV.Track();
@@ -41,90 +62,133 @@ BRAGV.Viewer = function(divName)
 		this.tracks["track5"] = new BRAGV.Track();
 		this.tracks["track6"] = new BRAGV.Track();
 	}*/
-	this.resetViewer();
-	this.drawTicks();
-	this.drawTracks(w);
+	this.verticalScroller.setValue(80);
 };
 
 BRAGV.Viewer.prototype = {
-		drawTicks : function()
+		draw : function()
 		{
+			var thisDraw = new Date().getTime();
+			this.lastdraw = thisDraw;
+			this.resetViewer(thisDraw);
+			this.drawTicks(thisDraw);
+			this.drawTracks(this.trackWidth, thisDraw);
+		},
+		drawTicks : function(thisDraw)
+		{
+			if(thisDraw < this.lastDraw) return; 
 			var start = 40;
 			var end = this.trackWidth + 39;
 			
-			ctx.beginPath();
-			ctx.moveTo(start, 10);
-			ctx.lineTo(start, 20);
-			ctx.stroke();
+			this.ctx.fillStyle = 'rgba(0,0,0,1)';
 			
-			ctx.beginPath();
-			ctx.moveTo(end, 10);
-			ctx.lineTo(end, 20);
-			ctx.stroke();
+			this.ctx.beginPath();
+			this.ctx.moveTo(start, 10);
+			this.ctx.lineTo(start, 20);
+			this.ctx.stroke();
+			
+			this.ctx.beginPath();
+			this.ctx.moveTo(end, 10);
+			this.ctx.lineTo(end, 20);
+			this.ctx.stroke();
 			
 			var txt = (this.offset).toString();
-			var wt = ctx.measureText(txt).width;
-			ctx.fillText(this.offset, start-wt, 10);
+			var wt = this.ctx.measureText(txt).width;
+			this.ctx.fillText(this.offset, start-wt, 10);
 			var txt = (this.offset + this.numBases).toString();
-			var wt = ctx.measureText(txt).width;
-			ctx.fillText(txt, end-wt, 10);
+			var wt = this.ctx.measureText(txt).width;
+			this.ctx.fillText(txt, end-wt, 10);
 		},
-		drawTracks : function(w)
+		drawTracks : function(w, thisDraw)
 		{
 			var i = 1;
 			var trackheight = 20;
 			var padding = 3;
-			
+			var labelWidth = this.labelWidth;
 			
 			for(var t in this.tracks)
 			{
+				if(thisDraw < this.lastDraw) return; 
 				var tracky  = i*(trackheight + padding);
-				ctx.fillStyle = 'rgba(80, 80, 80, 0.1)';
-				ctx.fillRect(40, tracky, w - 40, trackheight);
-				ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-				ctx.fillText(t, 5, ++i * (trackheight +	 padding) -10);
 				
+				this.ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+				this.ctx.fillText(t, 5, ++i * (trackheight + padding) -10);
+				
+				this.ctx.fillStyle = 'rgba(80, 80, 80, 0.1)';
+				this.ctx.fillRect(labelWidth, tracky, w, trackheight);
+				
+				this.lastpos = 0;
 				var features = this.tracks[t].features;
 				var count = features.length;
 				
+				this.baseWidth = this.trackWidth / this.numBases;
+				
 				for(var j = 0; j != count; j++)
 				{
-					if(features[j].s > this.offset + this.numBases) return;
+					if(thisDraw < this.lastDraw) return; 
+					if(features[j].s > this.offset + this.numBases) break;
 					if(features[j].e < this.offset) continue;
-					if(features[j].s % 3 != 1) continue;
 					
-					if(debug){
+					/*if(debug){
 						console.debug('drawing ' + features[j].i);
 						console.debug(this.baseWidth);
 						console.debug((40 + (features[j].s * this.baseWidth)));
 						console.debug(tracky);
 						console.debug((features[j].e - features[j].s) * this.baseWidth);
-					}
+					}*/
 					
-					ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-					ctx.fillRect(40 + (features[j].s * this.baseWidth), tracky, 40 + ((features[j].e - features[j].s)) * this.baseWidth, trackheight);
-					ctx.strokeRect(40 + (features[j].s * this.baseWidth), tracky, 40 + ((features[j].e - features[j].s)) * this.baseWidth, trackheight);
+					var start = labelWidth + (Math.max((features[j].s - this.offset) * this.baseWidth, 0));
+					var width = (features[j].e - Math.max(this.offset, features[j].s)) * this.baseWidth;
+					
+					if((start+width) < (this.lastpos + 1)) continue;
+					this.lastpos = start + width;
+					this.ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+					this.ctx.fillRect(start, tracky, width, trackheight);
+					//this.ctx.strokeRect(start, tracky, width, trackheight);
 				}
 			}
 		},
-		loadTracks : function(url)
+		loadAnnotation : function(url)
 		{
 			viewer = this;
 			$.getJSON(url, null, function(data)
-				{	
+			{	
 				 viewer.addTracks(data);
-				}
-			);
+			});
 		},
 		addTracks : function(obj)
 		{
-			if(!viewer.tracks["track1"])viewer.tracks["track1"] = new BRAGV.Track('track1');
-			viewer.tracks["track1"].features = obj;
-			this.drawTracks(this.trackWidth);
+			var count = obj.length;
+			for(var i = 0; i < count; i++)
+			{
+				this.c_length = Math.max(obj[i].length);
+				var id = obj[i].frame + '_' + obj[i].strand;
+				
+				if(!viewer.tracks[id])viewer.tracks[id] = new BRAGV.Track(id);
+				viewer.tracks[id].features = obj[i].features;
+			}
+			this.draw();
 		},
 		resetViewer : function()
 		{
-			ctx.clearRect(0,0, this.trackWidth + 40, 400);
+			this.ctx.clearRect(0,0, this.trackWidth + this.labelWidth, 400);
+		},
+		setOffset : function(val)
+		{
+			clearTimeout(this.nextDraw);
+			var d = this;
+			
+			val = Math.round((val / this.horizontalScroller.max) * (this.c_length - this.numBases));
+			
+			this.offset = Math.round(val);
+			this.nextDraw = setTimeout(function(){d.draw();}, this.numBases > 50000 ? 100 : 10);
+		},
+		setZoom : function(val)
+		{
+			clearTimeout(this.nextDraw);
+			var d = this;
+			this.numBases = Math.pow(10,Math.round(val / 20) + 1);
+			this.nextDraw = setTimeout(function(){d.draw();}, this.numBases > 50000 ? 100 : 10);
 		}
 };
 
@@ -135,6 +199,7 @@ BRAGV.Viewer.prototype = {
 BRAGV.Track = function(trackName)
 {
 	this.features = [];
+	this.isReverse = false;
 	
 	if(debug) {
 		function genPos(min, max)
@@ -154,3 +219,100 @@ BRAGV.Track = function(trackName)
 	}
 };
 
+BRAGV.Scroller = function(divName, dir)
+{
+	this.div = $('#' + divName);
+
+	this.direction = dir;
+	this.min = 0;
+	this.max = 100;
+	this.step = 10;
+	
+	if(dir == 'v')
+	{
+		this.div.prepend('<div class="bragv_scroller bragv_vertical"><div class="bragv_scroller_handle"></div><div class="bragv_scroller_ctl_lesser"></div><div class="bragv_scroller_ctl_greater"></div></div>');
+		$('.bragv_vertical').height($('.bragv_canvas').height() - 27);
+	}
+	else
+	{
+		this.div.append('<div class="bragv_scroller bragv_horizontal"><div class="bragv_scroller_handle"></div><div class="bragv_scroller_ctl_lesser"></div><div class="bragv_scroller_ctl_greater"></div></div>');
+	}
+	this.base = $('.bragv_scroller', this.div);
+	this.base = this.base[0];
+	
+	var bar = this;
+	$('.bragv_scroller_ctl_lesser', this.base).click(function(evt){
+		var newval = bar.getValue() - bar.step;
+		newval = newval - (newval % bar.step);
+		newval = Math.max(newval, bar.min);
+		bar.setValue(newval);
+	});
+	$('.bragv_scroller_ctl_greater', this.base).click(function(evt){
+		var newval = bar.getValue() + bar.step;
+		newval = newval + (newval % bar.step);
+		newval = Math.min(newval, bar.max);
+		bar.setValue(newval);
+	});
+	$('.bragv_scroller_handle', this.base).draggable({
+		containment : 'parent',
+		drag : function(event, ui)
+		{
+			bar.setValue(bar.getValue());
+			//console.debug(bar.getValue());
+		}
+	});
+	this.setValue(0);
+};
+
+BRAGV.Scroller.prototype = {
+	getValue: function()
+	{
+		var rawval;
+		var d_range;
+		var v_range = this.max - this.min;
+		
+		if(this.direction == 'v')
+		{ 
+			d_range = $(this.base).innerHeight() - 24; // inner width of slider div minus width of slider
+			rawval = $('.bragv_scroller_handle', this.base).css('top').replace('px','');
+			rawval = d_range - rawval;
+		}
+		else
+		{ 
+			d_range = $(this.base).innerWidth() - 24;
+			rawval = $('.bragv_scroller_handle', this.base).css('left').replace('px',''); 
+		}
+		 
+		var val = (rawval/d_range) * v_range;
+		val += this.min;
+		return val;
+	},
+	setValue : function(val)
+	{
+		var rawval;
+		var d_range;
+		var v_range = this.max - this.min;
+		var o_val = val;
+		if(this.direction == 'v')
+		{ 
+			val = this.max - val;
+			d_range = $(this.base).innerHeight() - 24; // inner width of slider div minus width of slider
+		}
+		else
+		{ 
+			d_range = $(this.base).innerWidth() - 24;
+		}
+		
+		rawval = (val / v_range) * d_range;
+		if(this.direction == 'v')
+		{ 
+			$('.bragv_scroller_handle', this.base).css('top', rawval + 'px'); 
+		}
+		else
+		{ 
+			$('.bragv_scroller_handle', this.base).css('left', rawval + 'px'); 
+		}
+		
+		if(this.onValueChange) this.onValueChange(o_val);
+	}
+};
