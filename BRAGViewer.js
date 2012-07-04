d7e7dfc6b3c41d8dcd5502f1d69f4e32d9075642
Chris Powell <chris.i.powell@gmail.com>
2012-07-04 11:04:08 +0100

    CSS fix in the example and indication of track direction

--- BRAGViewer.js
+++ BRAGViewer.js
@@ -49,6 +49,7 @@ BRAGV.Viewer = function(divName)
 		
 		vwr.setOffset(val);
 	};
+	
 	this.verticalScroller.onValueChange = function(val)
 	{
 		vwr.setZoom(val);
@@ -78,7 +79,7 @@ BRAGV.Viewer.prototype = {
 		{
 			if(thisDraw < this.lastDraw) return; 
 			var start = 40;
-			var end = this.trackWidth + 39;
+			var end = this.trackWidth + 20;
 			
 			this.ctx.fillStyle = 'rgba(0,0,0,1)';
 			
@@ -106,10 +107,13 @@ BRAGV.Viewer.prototype = {
 			var padding = 3;
 			var labelWidth = this.labelWidth;
 			
+			w = w - trackheight;
+			
 			for(var t in this.tracks)
 			{
 				if(thisDraw < this.lastDraw) return; 
 				var tracky  = i*(trackheight + padding);
+				var isReverse = this.tracks[t].isReverse;
 				
 				this.ctx.fillStyle = 'rgba(0, 0, 0, 1)';
 				this.ctx.fillText(t, 5, ++i * (trackheight + padding) -10);
@@ -117,11 +121,12 @@ BRAGV.Viewer.prototype = {
 				this.ctx.fillStyle = 'rgba(80, 80, 80, 0.1)';
 				this.ctx.fillRect(labelWidth, tracky, w, trackheight);
 				
+				
 				this.lastpos = 0;
 				var features = this.tracks[t].features;
 				var count = features.length;
 				
-				this.baseWidth = this.trackWidth / this.numBases;
+				this.baseWidth = w / this.numBases;
 				
 				for(var j = 0; j != count; j++)
 				{
@@ -129,28 +134,57 @@ BRAGV.Viewer.prototype = {
 					if(features[j].s > this.offset + this.numBases) break;
 					if(features[j].e < this.offset) continue;
 					
-					/*if(debug){
-						console.debug('drawing ' + features[j].i);
-						console.debug(this.baseWidth);
-						console.debug((40 + (features[j].s * this.baseWidth)));
-						console.debug(tracky);
-						console.debug((features[j].e - features[j].s) * this.baseWidth);
-					}*/
-					
 					var start = labelWidth + (Math.max((features[j].s - this.offset) * this.baseWidth, 0));
-					var width = (features[j].e - Math.max(this.offset, features[j].s)) * this.baseWidth;
+					var width = (Math.min(features[j].e, this.numBases + this.offset) - Math.max(this.offset, features[j].s)) * this.baseWidth;
 					
 					if((start+width) < (this.lastpos + 1)) continue;
 					this.lastpos = start + width;
 					this.ctx.fillStyle = 'rgba(255, 0, 0, 1)';
 					this.ctx.fillRect(start, tracky, width, trackheight);
+					
+					
 					//this.ctx.strokeRect(start, tracky, width, trackheight);
+				}
+				
+				this.ctx.fillStyle = 'rgba(200,200,200,0.3)';
+				this.ctx.beginPath();
+				if(isReverse)
+				{
+					this.ctx.moveTo(labelWidth, tracky);
+					this.ctx.lineTo(labelWidth * 0.7, tracky + (trackheight * .5));
+					this.ctx.lineTo(labelWidth, tracky + trackheight);
+					this.ctx.closePath();
+					this.ctx.fill();
+					
+					this.ctx.beginPath();
+					var x = labelWidth + w;
+					this.ctx.moveTo(x, tracky);
+					this.ctx.lineTo(x - labelWidth * 0.3, tracky + (trackheight * .5));
+					this.ctx.lineTo(x, tracky + trackheight);
+					this.ctx.closePath();
+					this.ctx.fill();
+				}
+				else
+				{
+					this.ctx.moveTo(labelWidth, tracky);
+					this.ctx.lineTo(labelWidth * 1.3, tracky + (trackheight * .5));
+					this.ctx.lineTo(labelWidth, tracky + trackheight);
+					this.ctx.closePath();
+					this.ctx.fill();
+					
+					this.ctx.beginPath();
+					var x = labelWidth + w;
+					this.ctx.moveTo(x, tracky);
+					this.ctx.lineTo(x + labelWidth * 0.3, tracky + (trackheight * .5));
+					this.ctx.lineTo(x, tracky + trackheight);
+					this.ctx.closePath();
+					this.ctx.fill();
 				}
 			}
 		},
 		loadAnnotation : function(url)
 		{
-			viewer = this;
+			var viewer = this;
 			$.getJSON(url, null, function(data)
 			{	
 				 viewer.addTracks(data);
@@ -158,6 +192,7 @@ BRAGV.Viewer.prototype = {
 		},
 		addTracks : function(obj)
 		{
+			var viewer = this;
 			var count = obj.length;
 			for(var i = 0; i < count; i++)
 			{
@@ -165,6 +200,7 @@ BRAGV.Viewer.prototype = {
 				var id = obj[i].frame + '_' + obj[i].strand;
 				
 				if(!viewer.tracks[id])viewer.tracks[id] = new BRAGV.Track(id);
+				viewer.tracks[id].isReverse = obj[i].strand < 0;
 				viewer.tracks[id].features = obj[i].features;
 			}
 			this.draw();
